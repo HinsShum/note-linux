8d8149e93ab63e53f7aa64672b0e3b137df2529b
HinsShum <hinsshum@qq.com>
2020-10-16 11:12:30 +0800

    <Tools-Modify>: 更改CMake的交叉编译配置文件，设置CMAKE_TRY_COMPILE_TARGET_TYPE为STATIC_LIBRARY来避免CMake测试编译时进行链接，避免构建错误

--- tools/CMake/Example_Embedded/toolchain_cortexm3.cmake
+++ tools/CMake/Example_Embedded/toolchain_cortexm3.cmake
@@ -6,13 +6,13 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR Arm)
 
 # set dir variables
-set(PROJECT_DIR "/home/cenyue/Desktop/note/note-linux/tools/CMake/Example_Embedded")
-set(CROSS_COMPILER_DIR "/usr/bin")
+set(CROSS_COMPILER_DIR "")
 
 # set compiler
-set(CMAKE_C_COMPILER "${CROSS_COMPILER_DIR}/arm-none-eabi-gcc")
-set(CMAKE_OBJCOPY "${CROSS_COMPILER_DIR}/arm-none-eabi-objcopy")
-set(CMAKE_SIZE "${CROSS_COMPILER_DIR}/arm-none-eabi-size")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY") # to avoid error when cmake -B build
+set(CMAKE_C_COMPILER "${CROSS_COMPILER_DIR}arm-none-eabi-gcc")
+set(CMAKE_OBJCOPY "${CROSS_COMPILER_DIR}arm-none-eabi-objcopy")
+set(CMAKE_SIZE "${CROSS_COMPILER_DIR}arm-none-eabi-size")
 
 # set definitions
 add_definitions(-DEFM32LG280F256 -DEFM32_HFXO_FREQ=16000000 -DCONFIG_PRINTK)
@@ -25,7 +25,7 @@ set(CMAKE_C_FLAGS_RELEASE "-O3")
 set(CMAKE_BUILD_TYPE "Debug")
 
 # set linker flags
-set(LINK_SCRIPT_FLAGS "-Wl,--gc-sections,-T${PROJECT_DIR}/config/linker.ld")
+set(LINK_SCRIPT_FLAGS "-Wl,--gc-sections,-T${PROJECT_SOURCE_DIR}/config/linker.ld")
 set(LINK_MAP_FLAGS "-Wl,-Map,${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map,--cref")
 set(LINK_MISC_FLAGS "-fno-exceptions -nostartfiles --specs=nosys.specs")
 set(CMAKE_EXE_LINKER_FLAGS "${LINK_MISC_FLAGS} ${LINK_SCRIPT_FLAGS} ${LINK_MAP_FLAGS}")
